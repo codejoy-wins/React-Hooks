8ee3fa82401934eea4538f0eb6eecbf12cfbeb23
Max <mjann@ucsc.edu>
2019-03-03 15:23:29 -0800

    hooks working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// fancy destructuring of props
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}></input>
+    </form>
+  )
+}
+
+
+
+// Todo alternate structures:
+
+// no destructuring
+function Xp(props){
+  return(
+    <div>XP{props.todo.text}</div>
+  )
+}
+// some
+function Xxp(props){
+  const { todo } = props;
+  return(
+    <div>XXP{todo.text}</div>
+  )
+}
+
+// more
+
+function Xxxp({todo}){
+  return <div>mage {todo.text}</div>
+}
+// most
+
+function Xxxxp({todo, index, key}){
+  return <div>mage {index}{todo.text}{key}</div>
+}
+
+//   MAIN APPLICATION 
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Go to the beach',
+      isCompleted: false,
+    },
+    {
+      text: 'Learn Calculus',
+      isCompleted: false,
+    },
+    {
+      text: 'Study Machine Learning',
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((thing,spot)=> (
+          <Todo key={spot} index={spot} todo={thing}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Fire.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Fire extends Component {
+  render() {
+
+
+
+
+
+    return (
+      <div>
+        Fire
+      </div>
+    )
+  }
+}
+
+export default Fire
