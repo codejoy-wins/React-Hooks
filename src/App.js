520e024e53fd18946f9d17405f35c3e842cc2447
Max <mjann@ucsc.edu>
2019-03-03 15:37:47 -0800

    cross things off

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 // fancy destructuring of props
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
       {todo.text}
+      <div>
+        <button onClick={()=> completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -84,11 +87,17 @@ function App() {
     setTodos(newTodos);
   }
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((thing,spot)=> (
-          <Todo key={spot} index={spot} todo={thing}/>
+          <Todo key={spot} index={spot} todo={thing} completeTodo={completeTodo}/>
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
